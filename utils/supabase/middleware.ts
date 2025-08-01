5fa7efb72b0dca8dfe3fb33777d854b89dd452a0
Dylan Anderson <anderson.dylan.522@gmail.com>
2025-08-01 14:52:58 -0400

    confirm email added to public route?

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -27,8 +27,7 @@ export async function updateSession(request: NextRequest) {
     data: { user },
   } = await supabase.auth.getUser()
 
-  console.log(user)
-  const publicPaths = ['/', '/login', '/auth', '/signup', '/privacy', '/terms']
+  const publicPaths = ['/', '/login', '/auth', '/signup', '/privacy', '/terms', '/confirm-email']
 
   const isPublicPath = publicPaths.some((path) =>
     request.nextUrl.pathname.startsWith(path)
