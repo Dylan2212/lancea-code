e29fbd220b8c27f361154098c7a2d16b7353752d
Dylan Anderson <anderson.dylan.522@gmail.com>
2025-09-24 17:26:20 -0400

    video works

--- src/app/lancrdashboard/projects/components/dropzone.tsx
+++ src/app/lancrdashboard/projects/components/dropzone.tsx
@@ -53,36 +53,35 @@ export default function ProjectGallery ({ files, setFiles, cover, setCover, setR
     )])
   }
 
-  function deleteFile (toDelete: number) {
-
+  function deleteFile(toDelete: number) {
     if (!files[toDelete].file) {
       const split = files[toDelete].url.split("/projects/")
       const filePath = split[1]
       setRemovedFiles(prev => [...prev, filePath])
     }
-    
+
     setFiles(prevFiles => {
-      const fileToDelete = prevFiles[toDelete];
+      const fileToDelete = prevFiles[toDelete]
       if (fileToDelete?.url) {
-        URL.revokeObjectURL(fileToDelete.url);
+        URL.revokeObjectURL(fileToDelete.url)
       }
 
-      const newFiles = prevFiles.filter((_, idx) => idx !== toDelete);
+      const newFiles = prevFiles.filter((_, idx) => idx !== toDelete)
 
-      return newFiles;
+      let newCover = cover
+      if (toDelete === cover) {
+        newCover = newFiles.length > 0 ? 0 : -1
+      } else if (toDelete < cover) {
+        newCover = cover - 1
+      }
+
+      setCover(newCover)
+
+      return newFiles
     })
-
-    setCover(prevCover => {
-      if (toDelete === prevCover) {
-        return 0
-      } else if (toDelete < prevCover) {
-        return prevCover - 1
-      }
-      return prevCover;
-    });
   }
 
-  const { getRootProps, getInputProps } = useDropzone({ onDrop })
+  const { getRootProps, getInputProps } = useDropzone({ onDrop, accept: {"image/*": [], "video/*": []}, maxSize: 100*1024*1024 })
 
   return (
     <div className="ml-2 mt-6">
@@ -98,23 +97,22 @@ export default function ProjectGallery ({ files, setFiles, cover, setCover, setR
 
         <Masonry breakpointCols={{default: 2, 768: 1}} className="my-masonry-grid py-10 px-6" columnClassName="my-masonry-grid_column">
           {files.map((file, idx) => (
-            <div key={file.id} className={`${cover === idx ? "ring ring-offset-4 ring-purple-600 rounded-lg" : ""} mb-6`}>
-              <div style={{aspectRatio: `${file.aspectRatio}` }} className={`overflow-hidden mx-auto rounded relative shadow-md border`}>
-                <Image
+            <div key={file.id} className={`${cover === idx ? "ring ring-offset-4 ring-purple-600 rounded-lg" : "hover:ring-offset-4 hover:ring hover:ring-purple-300 hover:rounded-lg"} mb-6 transition-all ease-in-out duration-200`}>
+              <div onClick={() => {
+                  setCover(idx)
+                  setFiles(prev => prev.map((f, i) =>  i === idx ? { ...f, aspectRatio: "4/3", file: f.file, name: f.name } : f))
+                  }} style={{aspectRatio: `${file.aspectRatio}` }} className={`overflow-hidden mx-auto rounded group relative shadow-md border`}>
+              {file.file?.type.startsWith("video/") ? (
+                <video src={file.url} controls className="object-cover w-full h-full"/>
+              ) :
+                (<Image
                   src={file.url}
                   className={`image-crisp object-cover`}
                   alt={file.name || "Uploaded File"}
                   fill
                   sizes="300px"
                 />
-                {cover !== idx && <div onClick={() => {
-                  setCover(idx)
-                  setFiles(prev => prev.map((f, i) =>  i === idx ? { ...f, aspectRatio: "4/3", file: f.file, name: f.name } : f))
-                  }} className="opacity-0 hover:opacity-100 absolute w-full hov-standrd h-full bg-black/50 top-0 flex justify-center items-center">
-                  <button className="text-white">
-                    Set As Cover
-                  </button>
-                </div>}
+              )}
                 <Trash2 onClick={() => deleteFile(idx)} className="absolute w-7 h-7 hover:text-red-600 hov-standrd top-2 right-2 bg-white/50 p-1 rounded-full z-20"/>
               </div>
               <div className="mt-3 mx-2 flex justify-around items-center border border-gray-500 shadow-sm rounded-lg py-1">
--- src/app/lancrdashboard/projects/components/project.tsx
+++ src/app/lancrdashboard/projects/components/project.tsx
@@ -12,11 +12,16 @@ type MyProps = {
 }
 
 export default function Project ({ project, setShowDeleteModal, idx }: MyProps) {
-  
+  if (!project.cover?.coverUrl) return
+  const isVideo = project.cover?.coverUrl.split('.').pop()?.match(/mp4|mov|webm/i)
   return (
     <div className="w-11/12 rounded-lg group mx-auto lg:mx-0 md:w-[300px]">
       <div className="relative aspect-[4/3] w-full">
-        {project.cover?.coverUrl ? <Image alt="project cover" priority className={`object-cover image-crisp aspect-[4/3] rounded-lg shadow-md shadow-gray-400`} fill sizes="300px" src={project.cover.coverUrl} /> : null}
+        {isVideo ? (
+          <video src={project.cover.coverUrl} controls className="object-cover aspect-[4/3]"/>
+        ) :
+          (<Image alt="project cover" priority className={`object-cover image-crisp aspect-[4/3] rounded-lg shadow-md shadow-gray-400`} fill sizes="300px" src={project.cover.coverUrl} />)
+          }
         <div className="flex transition-all duration-300 bg-black/50 rounded-b-lg z-10 absolute w-full bottom-0 h-16 items-center justify-end gap-4 pr-5
         lg:pointer-events-none lg:group-hover:pointer-events-auto lg:opacity-0 lg:group-hover:opacity-100">
           <Link href={`/lancrdashboard/projects/add+editproject?action=edit&idx=${idx}`} className="py-2 px-4 text-white hover:bg-purple-500 hov-standrd bg-purple-600 rounded-lg">Edit</Link>
