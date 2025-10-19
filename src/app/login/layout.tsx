7e44f00682776b33cc7c96407141a78f965186de
Dylan Anderson <anderson.dylan.522@gmail.com>
2025-10-19 15:12:46 -0400

    icon

--- src/app/login/layout.tsx
+++ src/app/login/layout.tsx
@@ -1,6 +1,10 @@
 export const metadata = {
   title: "Log In or Sign Up | Lancrly",
   description: "Access your Lancrly account or create a new one to manage your freelancing links easily.",
+  icons: {
+    icon: "/favicon.png",
+    shortcut: "/favicon.png"
+  }
 };
 
 export default function LoginLayout({ children }: { children: React.ReactNode }) {
