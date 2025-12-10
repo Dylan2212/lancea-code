e633be7ed036056af71ee4c52eb774f1c81612c6
Dylan Anderson <anderson.dylan.522@gmail.com>
2025-12-10 08:50:14 -0500

    images

--- src/middleware.ts
+++ src/middleware.ts
@@ -53,5 +53,8 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
+  matcher: [
+    "/((?!api/|_next/|favicon.ico).*)"
+  ],
 }
+
