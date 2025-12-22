7827ecabd9cc29d638f2b01cc35e5c5669aa107d
Dylan Anderson <anderson.dylan.522@gmail.com>
2025-12-22 18:39:16 -0500

    unused file

--- src/app/lancrdashboard/profile/utils/checkForChanges.ts
+++ src/app/lancrdashboard/profile/utils/checkForChanges.ts
@@ -1,9 +1,9 @@
-import { useAdditionalLinksStore } from "@/lib/store/useAdditionalLinksStore"
-import { useUserStore } from "@/lib/store/useUserStore"
-export function checkForChanges () {
-  const { bio, username, title, handle, changedProfileImage, socialLinks, userId } = useUserStore.getState()
-  const additionalLinks = useAdditionalLinksStore.getState().links
-  const changedBio = checkObjChanges({ bio, username, title, handle }, originalUserData)
-  const changedSocialLinks = checkObjChanges(socialLinks, originalSocialLinks)
-  const changedAdditionalLinks = checkAdditionalLinksChanges(additionalLinks, originalAdditionalLinks, userId)
-}
+//import { useAdditionalLinksStore } from "@/lib/store/useAdditionalLinksStore"
+//import { useUserStore } from "@/lib/store/useUserStore"
+//export function checkForChanges () {
+//  const { bio, username, title, handle, changedProfileImage, socialLinks, userId } = useUserStore.getState()
+//  const additionalLinks = useAdditionalLinksStore.getState().links
+//  const changedBio = checkObjChanges({ bio, username, title, handle }, originalUserData)
+//  const changedSocialLinks = checkObjChanges(socialLinks, originalSocialLinks)
+//  const changedAdditionalLinks = checkAdditionalLinksChanges(additionalLinks, originalAdditionalLinks, userId)
+//}
